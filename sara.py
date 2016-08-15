fd9632dede4a10ec4d3939aa0f1c054431a47d18
Mohamed Saif <mscoder12@gmail.com>
2016-08-15 13:28:17 +0300

    Update sara.py

--- sara.py
+++ sara.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-import sys,os,webbrowser
+import sys,os,webbrowser,getpass,urllib
+from bs4 import BeautifulSoup
 fc = sys.argv[1]
-
 if (fc == "hi"):
-	print("Hello "+str(os.system("whoami")))
+	print("Hello "+getpass.getuser())
 elif (fc == "youtube"):
 	url = "https://www.youtube.com/results?search_query="+sys.argv[2]
 	webbrowser.open_new_tab(url)
@@ -13,14 +13,38 @@ elif (fc == "google"):
 elif (fc == "twitter"):
 	url = "chromium-browser https://www.twitter.com/"+sys.argv[2]
 	webbrowser.open_new_tab(url)
+elif (fc == "sc"):
+	url = "https://soundcloud.com/search?q="+sys.argv[2]
+	webbrowser.open_new_tab(url)
+elif (fc == "lyrics"):
+	url = "http://www.azlyrics.com/lyrics/"+sys.argv[2]+"/"+sys.argv[3]+".html"
+	html = urllib.urlopen(url).read()
+	soup = BeautifulSoup(html)
+	# kill all script and style elements
+	for script in soup(["script", "style"]):
+		script.extract()    # rip it out
+
+	# get text
+	text = soup.get_text()
+
+	# break into lines and remove leading and trailing space on each
+	lines = (line.strip() for line in text.splitlines())
+	# break multi-headlines into a line each
+	chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+	# drop blank lines
+	text = '\n'.join(chunk for chunk in chunks if chunk)
+
+	print(text)
 elif (fc == "time"):
-	os.system("date +%Y-%m-%d::%H:%M:%S")
+	os.system("date +%Y-%m-%d**%H:%M:%S")
 elif (fc == "download"):
 	os.system("wget -c "+sys.argv[2])
-elif (fc == "sing"):
-	os.system("rhythmbox "+sys.argv[2]+".mp3")
 elif (fc == "grab"):
 	print("Grabbing Entire Website Content For Offline Use ...")
+elif (fc == "nzli"):
+	os.system("youtube-dl "+sys.argv[2])
 elif (fc == "about"):
 	os.system("notify-send 'Hi , I am Sara , Your New Girlfriend <3'")
-	print("My name is Sara , I am a Terminal Bot Helps You to do almost any thing in SemiCode OS , I Can Search Google and Youtube or help you to Use Twitter and Facebook For You :)")
+	print("Hi , I am Sara , Your New Girlfriend <3 ")
+elif (fc == "help"):
+	print("* Available Commands For Sara : \n 1- hi \n 2- youtube \n 3- google \n 4- twitter \n 5- time \n 6- download \n 7- grab \n 8- about \n 9- sc ")
